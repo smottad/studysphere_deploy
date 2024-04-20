88d131467525b6c04d0fbe151e00a139f8715b5a
Anderson David Morales Chila <amoralesch@unal.edu.co>
2024-04-20 17:42:53 -0500

    Vista de creación de asignaturas

--- lib/Controladores/controlador_crear_asignatura.dart
+++ lib/Controladores/controlador_crear_asignatura.dart
@@ -1,3 +1,36 @@
 import 'package:flutter/material.dart';
 
 var name = TextEditingController();
+var initial_date = TextEditingController();
+var final_date = TextEditingController();
+
+class ControllerCalendar {
+  DateTime dateTimeVar;
+  String label= "";
+
+  ControllerCalendar (this.dateTimeVar, this.label);
+
+  DateTime compareDate(DateTime selectedDate, DateTime otherDate) {
+    if (selectedDate.isBefore(otherDate)) {
+      return selectedDate;
+    }
+
+    return otherDate;
+  }
+
+  void setDateTimeVar(DateTime date) {
+    dateTimeVar = date;
+  }
+
+  DateTime getDateTimeVar() {
+    return dateTimeVar;
+  }
+
+  void setLabel(String label) {
+    this.label = label;
+  }
+
+  String getLabel() {
+    return label;
+  }
+}
--- lib/Vistas/crear_asignatura.dart
+++ lib/Vistas/crear_asignatura.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:studysphere/Componentes/text_forms.dart';
 import 'package:studysphere/Controladores/controlador_crear_asignatura.dart';
+import 'package:studysphere/Componentes/select_days.dart';
+import 'package:studysphere/Componentes/app_bar.dart';
+
+ControllerCalendar controllerCalendarInit = ControllerCalendar(DateTime.now(), 'Fecha de inicio');
+ControllerCalendar controllerCalendarFinal = ControllerCalendar(DateTime.now().subtract(const Duration(days: 1)), 'Fecha de Finalización');
 
 class CrearAsignatura extends StatelessWidget {
   const CrearAsignatura({super.key});
@@ -9,12 +13,15 @@ class CrearAsignatura extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
     return Scaffold(
+      appBar: appBar(context, 'Crear asignaturas', color: 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             textFormulario(context, name, "Nombre de asignatura"),
-            const DatePickerExample(restorationId: 'main',),
+            const InitCalendar(restorationId: 'main',),
+            const FinalCalendar(restorationId: 'main',),
+            const SelectDays(),
             // DatePickerDialog(
             //   restorationId: 'date_picker_dialog',
             //   initialEntryMode: DatePickerEntryMode.input,
@@ -29,24 +36,24 @@ class CrearAsignatura extends StatelessWidget {
   }
 }
 
-class DatePickerExample extends StatefulWidget {
-  const DatePickerExample({super.key, this.restorationId});
+class InitCalendar extends StatefulWidget {
+  const InitCalendar({super.key, this.restorationId,});
 
   final String? restorationId;
 
   @override
-  State<DatePickerExample> createState() => _DatePickerExampleState();
+  State<InitCalendar> createState() => _InitCalendar();
 }
 
 /// RestorationProperty objects can be used because of RestorationMixin.
-class _DatePickerExampleState extends State<DatePickerExample>
-    with RestorationMixin {
+class _InitCalendar extends State<InitCalendar> with RestorationMixin {
   // In this example, the restoration ID for the mixin is passed in through
   // the [StatefulWidget]'s constructor.
   @override
   String? get restorationId => widget.restorationId;
+
   final RestorableDateTime _selectedDate =
-      RestorableDateTime(DateTime.now());
+      RestorableDateTime(controllerCalendarInit.getDateTimeVar());
   late final RestorableRouteFuture<DateTime?> _restorableDatePickerRouteFuture =
       RestorableRouteFuture<DateTime?>(
     onComplete: _selectDate,
@@ -72,7 +79,7 @@ class _DatePickerExampleState extends State<DatePickerExample>
           initialDate: DateTime.fromMillisecondsSinceEpoch(arguments! as int),
           firstDate: DateTime.now(),
           lastDate: DateTime(2060),
-          fieldLabelText: 'Fecha de inicio:',
+          fieldLabelText: controllerCalendarInit.getLabel(),
         );
       },
     );
@@ -89,40 +96,182 @@ class _DatePickerExampleState extends State<DatePickerExample>
     if (newSelectedDate != null) {
       setState(() {
         _selectedDate.value = newSelectedDate;
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(
-              'Selected: ${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}'),
-        ));
+        controllerCalendarInit.setDateTimeVar(newSelectedDate);
+
+        if (controllerCalendarFinal.getDateTimeVar().isBefore(_selectedDate.value)) {
+          controllerCalendarFinal.setDateTimeVar(newSelectedDate.add(const Duration(days: 1)));
+        }
+        // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        //   content: Text(
+        //       'Selected: ${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}'),
+        initial_date.text = '${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}';
       });
     }
   }
-    @override
+  @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column( 
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(6.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width*0.7,
-                child: TextField(
-                  enabled: false,
-                  decoration: InputDecoration(
-                    fillColor: Theme.of(context).colorScheme.surface,
-                    labelText: "Ingrese la fecha de inicio",
-                    border: const OutlineInputBorder(),
-                  )
+      child: Column( 
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min, 
+              children:[ 
+                SizedBox(
+                  width: MediaQuery.of(context).size.width*0.5,
+                  child: TextField(
+                    keyboardType: TextInputType.datetime,
+                    enabled: false,
+                    controller: initial_date,
+                    style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+                    decoration: InputDecoration(
+                      fillColor: Theme.of(context).colorScheme.surface,
+                      labelText: controllerCalendarInit.getLabel(),
+                      border: const OutlineInputBorder(),
+                      labelStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                      
+                      disabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.onSecondary,
+                        ),
+                      ), 
+                      
+                    ),
+                  ),
                 ),
-              ),
+                IconButton(
+                  onPressed: () {
+                    _restorableDatePickerRouteFuture.present();
+                  },
+                  icon: const Icon(Icons.calendar_month),
+                    // label: Text('${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}'),
+                ),
+              ],
             ),
-            IconButton(
-              onPressed: () {
-                _restorableDatePickerRouteFuture.present();
-              },
-              icon: const Icon(Icons.calendar_month),
-              // label: Text('${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FinalCalendar extends StatefulWidget {
+  const FinalCalendar({super.key, this.restorationId,});
+
+  final String? restorationId;
+
+  @override
+  State<FinalCalendar> createState() => _FinalCalendar();
+}
+
+/// RestorationProperty objects can be used because of RestorationMixin.
+class _FinalCalendar extends State<FinalCalendar> with RestorationMixin {
+  // In this example, the restoration ID for the mixin is passed in through
+  // the [StatefulWidget]'s constructor.
+
+  @override
+  String? get restorationId => widget.restorationId;
+
+  final RestorableDateTime _selectedDate =
+      RestorableDateTime(controllerCalendarFinal.getDateTimeVar());
+  late final RestorableRouteFuture<DateTime?> _restorableDatePickerRouteFuture =
+      RestorableRouteFuture<DateTime?>(
+    onComplete: _selectDate,
+    onPresent: (NavigatorState navigator, Object? arguments) {
+      return navigator.restorablePush(
+        _datePickerRoute,
+        arguments: _selectedDate.value.millisecondsSinceEpoch,
+      );
+    },
+  );
+
+  @pragma('vm:entry-point')
+  static Route<DateTime> _datePickerRoute(
+    BuildContext context,
+    Object? arguments,
+  ) {
+    return DialogRoute<DateTime>(
+      context: context,
+      builder: (BuildContext context) {
+        return DatePickerDialog(
+          restorationId: 'date_picker_dialog',
+          initialEntryMode: DatePickerEntryMode.calendar,
+          initialDate: controllerCalendarFinal.getDateTimeVar(),
+          firstDate: controllerCalendarInit.getDateTimeVar().add(const Duration(days: 1)),
+          lastDate: DateTime(2060),
+          fieldLabelText: controllerCalendarFinal.getLabel(),
+        );
+      },
+    );
+  }
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_selectedDate, 'selected_date');
+    registerForRestoration(
+        _restorableDatePickerRouteFuture, 'date_picker_route_future');
+  }
+
+  void _selectDate(DateTime? newSelectedDate) {
+    if (newSelectedDate != null) {
+      setState(() {
+        _selectedDate.value = newSelectedDate;
+        controllerCalendarFinal.setDateTimeVar(newSelectedDate);
+        // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        //   content: Text(
+        //       'Selected: ${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}'),
+        final_date.text = '${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}';
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column( 
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min, 
+              children:[ 
+                SizedBox(
+                  width: MediaQuery.of(context).size.width*0.5,
+                  child: TextField(
+                    keyboardType: TextInputType.datetime,
+                    enabled: false,
+                    controller: final_date,
+                    style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+                    decoration: InputDecoration(
+                      fillColor: Theme.of(context).colorScheme.surface,
+                      labelText: controllerCalendarFinal.getLabel(),
+                      border: const OutlineInputBorder(),
+                      labelStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                      
+                      disabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.onSecondary,
+                        ),
+                      ), 
+                      
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    _restorableDatePickerRouteFuture.present();
+                  },
+                  icon: const Icon(Icons.calendar_month),
+                    // label: Text('${_selectedDate.value.day}/${_selectedDate.value.month}/${_selectedDate.value.year}'),
+                ),
+              ],
             ),
-          ],
+          ),
+        ],
       ),
     );
   }
