93e051484b0de87cb325b860db01bb276a42b05e
smottad <55636717+smottad@users.noreply.github.com>
2024-05-08 11:15:47 -0500

    ver proyectos traidos desde la base de datos

--- lib/Controladores/controlador_ver_proyectos.dart
+++ lib/Controladores/controlador_ver_proyectos.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:studysphere/Servicios/servicio_proyecto.dart';
 
-goEditProject(BuildContext context, String nameProject,) {
-  Navigator.pushNamed(context, '/inicio/editar_proyectos', arguments: {"nameProject": nameProject,});
+goEditProject(
+  BuildContext context,
+  String nameProject,
+) {
+  Navigator.pushNamed(context, '/inicio/editar_proyectos', arguments: {
+    "nameProject": nameProject,
+  });
 }
 
-goVerProyectosPasados(BuildContext context,) {
-  Navigator.pushNamed(context, '/inicio/proyectos_pasados',);
+goVerProyectosPasados(
+  BuildContext context,
+) {
+  Navigator.pushNamed(
+    context,
+    '/inicio/proyectos_pasados',
+  );
 }
 
-goVeProyectosActuales(BuildContext context,) {
-  Navigator.pushNamed(context, '/inicio/proyectos',);
+goVeProyectosActuales(
+  BuildContext context,
+) {
+  Navigator.pushNamed(
+    context,
+    '/inicio/proyectos',
+  );
+}
+
+obtenerNombresProyectos(BuildContext context) {
+  final servicioProyecto = ServicioRegistroProyectoBaseDatos();
+  return servicioProyecto.obtenerNombresProyectosPorUsuario(context);
 }
