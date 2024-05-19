9c3f33bed29b05fcce114d6eeadc98e9cedd41ba
smottad <55636717+smottad@users.noreply.github.com>
2024-05-19 18:20:48 -0500

    Crear asignaturas, y ver asignaturas completo

--- lib/Controladores/controlador_crear_asignatura.dart
+++ lib/Controladores/controlador_crear_asignatura.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:studysphere/Componentes/select_days.dart';
+import 'package:studysphere/Servicios/servicio_asignatura.dart';
 
 var name = TextEditingController();
 var initial_date = TextEditingController();
@@ -6,9 +8,9 @@ var final_date = TextEditingController();
 
 class ControllerCalendar {
   DateTime dateTimeVar;
-  String label= "";
+  String label = "";
 
-  ControllerCalendar (this.dateTimeVar, this.label);
+  ControllerCalendar(this.dateTimeVar, this.label);
 
   DateTime compareDate(DateTime selectedDate, DateTime otherDate) {
     if (selectedDate.isBefore(otherDate)) {
@@ -39,4 +41,44 @@ class ControllerTime {
   TimeOfDay? time;
 
   ControllerTime({this.time});
+}
+
+String _timeOfDayToString(TimeOfDay time) {
+  final now = DateTime.now();
+  final dt = DateTime(now.year, now.month, now.day, time.hour, time.minute);
+  return dt.toIso8601String().split('T')[1];
+}
+
+void guardarAsignatura(
+  BuildContext context,
+  String nombre,
+  DateTime fechaDeInicio,
+  DateTime fechaDeFin,
+  List<String> dias,
+  horaDeInicio,
+  horaDeFin,
+) async {
+  final ServicioBaseDatosAsignatura servicioBaseDatos =
+      ServicioBaseDatosAsignatura();
+
+  try {
+    print('Guardando asignatura');
+    await servicioBaseDatos.saveAsignatura(
+      context: context,
+      nombre: nombre,
+      fechaDeInicio: fechaDeInicio,
+      fechaDeFin: fechaDeFin,
+      dias: dias,
+      horaDeInicio: horaDeInicio,
+      horaDeFin: horaDeFin,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Asignatura guardada con éxito')),
+    );
+  } catch (e) {
+    print('Error al guardar la asignatura: $e');
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Error al guardar la asignatura: $e')),
+    );
+  }
 }
