5c57dd257430c6d5fe88ce026ed940e7f3fc00b2
brrodriguezd <brrodriguezd@unal.edu.co>
2024-06-11 12:27:37 -0500

    correccion de texto

--- lib/Vistas/pagina_inicio.dart
+++ lib/Vistas/pagina_inicio.dart
@@ -216,7 +216,7 @@ class PaginaInicio extends StatelessWidget {
     return Card(
       child: Column(
         children: [
-          const Text("Próximas tareas"),
+          const Text("Próximos Examenes"),
           FutureBuilder(
               future: obtenerNombresExamenes(),
               builder: (context, snapshot) {
@@ -255,7 +255,7 @@ class PaginaInicio extends StatelessWidget {
     return Card(
       child: Column(
         children: [
-          const Text("Próximas tareas"),
+          const Text("Próximas Reuniones"),
           FutureBuilder(
               future: obtenerNombresReuniones(),
               builder: (context, snapshot) {
