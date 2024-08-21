5196962aac154543fc3c6f22e8a99094c8d373f7
smottad <55636717+smottad@users.noreply.github.com>
2024-08-21 18:10:28 -0500

    Arreglo inicio sesion con google

--- lib/Vistas/iniciar_sesion.dart
+++ lib/Vistas/iniciar_sesion.dart
@@ -2,9 +2,88 @@ import 'package:flutter/material.dart';
 import 'package:studysphere/Componentes/boton.dart';
 import 'package:studysphere/Componentes/text_forms.dart';
 import 'package:studysphere/Controladores/controlador_iniciar_sesion.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:studysphere/Vistas/pagina_inicio.dart';
 
-class IniciarSesion extends StatelessWidget {
-  const IniciarSesion({super.key});
+class IniciarSesion extends StatefulWidget {
+  const IniciarSesion({Key? key}) : super(key: key);
+
+  @override
+  _IniciarSesionState createState() => _IniciarSesionState();
+}
+
+class _IniciarSesionState extends State<IniciarSesion> {
+  @override
+  void initState() {
+    _setupAuthListener();
+    super.initState();
+  }
+
+  void _setupAuthListener() {
+    supabase.auth.onAuthStateChange.listen((data) {
+      final event = data.event;
+      if (event == AuthChangeEvent.signedIn) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const PaginaInicio(),
+          ),
+        );
+      }
+    });
+  }
+
+  final supabase = Supabase.instance.client;
+  Future<AuthResponse> _googleSignIn() async {
+    try {
+      /// TODO: update the Web client ID with your own.
+      ///
+      /// Web Client ID that you registered with Google Cloud.
+      // const webClientId =
+      //     '790357339959-ou6j3phs7k166d0mugf0f997epf2ank4.apps.googleusercontent.com';
+
+      /// TODO: update the iOS client ID with your own.
+      ///
+      /// iOS Client ID that you registered with Google Cloud.
+      // const iosClientId = 'my-ios.apps.googleusercontent.com';
+
+      // Google sign in on Android will work without providing the Android
+      // Client ID registered on Google Cloud.
+
+      final GoogleSignIn googleSignIn = GoogleSignIn(
+        clientId:
+            "770206202095-6u07tb5ja5ajsbf2bkac4oamsdq11op7.apps.googleusercontent.com",
+        // serverClientId: webClientId,
+        scopes: [
+          'email',
+          'https://www.googleapis.com/auth/contacts.readonly',
+        ],
+      );
+
+      final googleUser = await googleSignIn.signIn();
+      final googleAuth = await googleUser!.authentication;
+      final accessToken = googleAuth.accessToken;
+      final idToken = googleAuth.idToken;
+
+      // if (accessToken == null) {
+      //   throw 'No Access Token found.';
+      // }
+
+      print("aca");
+      print("idToken: $idToken");
+      print("aca2");
+      print("accessToken: $accessToken");
+
+      return supabase.auth.signInWithIdToken(
+        provider: OAuthProvider.google,
+        idToken: idToken ?? '',
+        accessToken: accessToken,
+      );
+    } catch (e) {
+      print('Error signing in with Google: $e');
+      return Future.error(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +133,22 @@ class IniciarSesion extends StatelessWidget {
               ),
               textFormulario(context, password, "Contraseña", oscurecer: true),
               boton(context, "Iniciar sesión", iniciarSesion),
-              boton(context, "Iniciar con Google", iniciarSesion),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await _googleSignIn();
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                            'No se pudo obtener un token de acceso desde Google. Verifica tu cuenta de Google.'),
+                        duration: Duration(seconds: 3), // Duración del mensaje
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Google login'),
+              ),
               TextButton(
                 onPressed: () => irRegistro(context),
                 child: Text(
